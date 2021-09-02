26a189397b847f69964740c9bb88629ac20750a5
Fedor <fedor@dps.uibk.ac.at>
2021-09-02 13:16:16 +0200

    coarse integration skeletton

---
+++ src/main/java/edge/discovery/modules/LNResourcesModule.java
@@ -0,0 +1,17 @@
+package edge.discovery.modules;
+
+import at.uibk.dps.ee.guice.modules.ResourceModule;
+import edge.discovery.LocalNetworkResources;
+
+/**
+ * Configures the classes for the init and cleanup of local network resources.
+ * 
+ * @author Fedor Smirnov
+ */
+public class LNResourcesModule extends ResourceModule {
+
+  @Override
+  protected void config() {
+    addLocalResources(LocalNetworkResources.class);
+  }
+}
