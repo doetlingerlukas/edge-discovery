91228ce22d10623606d057c2cf1e9955a9e2dc82
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2021-09-20 22:53:46 +0200

    Add alternative discovery method.

--- src/main/java/edge/discovery/DiscoverySearch.java
+++ src/main/java/edge/discovery/DiscoverySearch.java
@@ -2,10 +2,12 @@ package edge.discovery;
 
 import com.google.inject.Inject;
 import io.vertx.core.json.JsonArray;
+import org.apache.commons.net.util.SubnetUtils;
 
 import java.io.IOException;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -17,7 +19,7 @@ import java.util.stream.Collectors;
  */
 public class DiscoverySearch {
 
-  private List<InetAddress> subnets = new ArrayList<>();
+  private List<InterfaceAddress> subnets = new ArrayList<>();
 
   /**
    * Constructs a list of broadcast addresses for every network interface.
@@ -30,8 +32,7 @@ public class DiscoverySearch {
           try {
             if (i.isUp()) {
               this.subnets.addAll(i.getInterfaceAddresses().stream()
-                .map(InterfaceAddress::getBroadcast)
-                .filter(Objects::nonNull)
+                .filter(si -> si.getAddress() instanceof Inet4Address)
                 .collect(Collectors.toList()));
             }
           } catch (SocketException e) {
@@ -47,7 +48,9 @@ public class DiscoverySearch {
    * Send a broadcast to all subnets.
    */
   public void broadcast() {
-    this.subnets
+    this.subnets.stream()
+      .map(InterfaceAddress::getBroadcast)
+      .filter(Objects::nonNull)
       .forEach(s -> {
         try {
           var socket = new DatagramSocket();
@@ -64,10 +67,42 @@ public class DiscoverySearch {
       });
   }
 
+  /**
+   * Send a UDP packet to all IP addresses in each network interface's subnet.
+   * Use only if UDP broadcast is blocked on network.
+   */
+  public void sendToAll() {
+    this.subnets.stream()
+      .filter(si -> !si.getAddress().isLoopbackAddress())
+      .forEach(si -> {
+
+        var address = si.getAddress().getHostAddress();
+        var prefix = si.getNetworkPrefixLength();
+
+        var subnetUtils = new SubnetUtils(address + "/" + prefix);
+        Arrays.stream(subnetUtils.getInfo().getAllAddresses())
+          .forEach(a -> {
+            try {
+              var socket = new DatagramSocket();
+
+              var buffer = Constants.broadcastAvailableMessage.getBytes();
+              var packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(a), Constants.broadcastPort);
+
+              socket.send(packet);
+              socket.close();
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+          });
+      });
+  }
+
   public JsonArray getSubnetsAsJson() {
     var json = new JsonArray();
 
     subnets.stream()
+      .map(InterfaceAddress::getBroadcast)
+      .filter(Objects::nonNull)
       .map(InetAddress::getHostAddress)
       .forEach(json::add);
 
